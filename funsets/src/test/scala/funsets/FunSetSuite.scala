e58554690a12f5c52b7a3d07733a546d9ee4d757
Artiom Negura <artiom.negura@telrock.com>
2015-06-02 19:05:25 +0300

    Week2 : added solutions for the first part of the assignment (2.1)

--- funsets/src/test/scala/funsets/FunSetSuite.scala
+++ funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -86,7 +86,7 @@ class FunSetSuite extends FunSuite {
    * Once you finish your implementation of "singletonSet", exchange the
    * function "ignore" by "test".
    */
-  ignore("singletonSet(1) contains 1") {
+  test("singletonSet(1) contains 1") {
     
     /**
      * We create a new instance of the "TestSets" trait, this gives us access
@@ -101,9 +101,12 @@ class FunSetSuite extends FunSuite {
     }
   }
 
-  ignore("union contains all elements") {
+  test("union contains all elements") {
     new TestSets {
       val s = union(s1, s2)
+      println("s1:"+FunSets.contains(s1, 1)+FunSets.contains(s1, 2)+FunSets.contains(s1, 3))
+      println("s2:"+FunSets.contains(s2, 1)+FunSets.contains(s2, 2)+FunSets.contains(s2, 3))
+      println("s:"+FunSets.contains(s, 1)+FunSets.contains(s, 2)+FunSets.contains(s, 3))
       assert(contains(s, 1), "Union 1")
       assert(contains(s, 2), "Union 2")
       assert(!contains(s, 3), "Union 3")
